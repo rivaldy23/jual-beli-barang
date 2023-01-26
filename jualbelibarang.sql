1408ba72a8c7b0a91ccc216991e7050ebf63aa1a
rivaldy23 <123617892+rivaldy23@users.noreply.github.com>
2023-01-26 12:51:57 +0700

    Add files via upload

---
+++ jualbelibarang.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 26, 2023 at 06:49 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jualbelibarang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_admin`
+--
+
+CREATE TABLE `data_admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(100) NOT NULL,
+  `status_admin` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_admin`
+--
+
+INSERT INTO `data_admin` (`id_admin`, `nama_admin`, `status_admin`) VALUES
+(2, 'aldi', 'aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_barang`
+--
+
+CREATE TABLE `data_barang` (
+  `id_barang` int(11) NOT NULL,
+  `kondisi_barang` varchar(200) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_barang`
+--
+
+INSERT INTO `data_barang` (`id_barang`, `kondisi_barang`, `nama_barang`, `stok`) VALUES
+(2, 'baru', 'mask', 200),
+(3, 'baru', 'tumblr', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_pelanggan`
+--
+
+CREATE TABLE `data_pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `nama_pelanggan` varchar(100) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_pelanggan`
+--
+
+INSERT INTO `data_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `alamat`) VALUES
+(2, 'aldy', 'malang'),
+(3, 'eqi', 'malang');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_admin`
+--
+ALTER TABLE `data_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `data_pelanggan`
+--
+ALTER TABLE `data_pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_admin`
+--
+ALTER TABLE `data_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `data_pelanggan`
+--
+ALTER TABLE `data_pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
